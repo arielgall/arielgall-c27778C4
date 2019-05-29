87bd39818a825cba1d6c9a31779b205d6eab665c
Ariel Gall <arielgall16@gmail.com>
2019-05-29 12:54:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# arielgall-chatapp
---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/9a1c20b2-28ce-4773-a897-082fc2023671/token";
+const instanceLocator = "v1:us1:9a1c20b2-28ce-4773-a897-082fc2023671";
+
+export { tokenUrl, instanceLocator }
